1b07f2bc5a91a13f87e60e77207c80aafa693f96
zhangjinppp <1259385807@qq.com>
2018-10-05 19:20:09 -0700

    hello

--- test1.c
+++ test1.c
@@ -4,12 +4,9 @@ int main()
   int a=0;
   int d=0;
   int g=3;
-<<<<<<< HEAD
   int d=5;
-=======
-  int d=4;
   int g=9;
->>>>>>> eb23de8896a04059d5a3d3ac5d97c3125c17e6c2
+  int kd=3;
   printf("%d ",a);
   return 0;
 }
